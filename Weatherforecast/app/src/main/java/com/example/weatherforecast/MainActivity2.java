98091dc6891b6b8c1411d0177882da33f0d98d59
Nguyễn Ngọc Trung_19125126 <64719609+Pkchu4869@users.noreply.github.com>
2021-07-14 11:06:27 +0700

    Update MainActivity2.java

--- Weatherforecast/app/src/main/java/com/example/weatherforecast/MainActivity2.java
+++ Weatherforecast/app/src/main/java/com/example/weatherforecast/MainActivity2.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,12 +16,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
     String CityName="";
+    ImageView imgback;
+    TextView txtName;
+    ListView lv;
+    CustomAdapter customAdapter;
+    ArrayList<Weather> weatherArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        anhxa();
         Intent intent=getIntent();
         String city=intent.getStringExtra("name");
         Log.d("Result","Trandmitted Data"+city);
@@ -29,6 +40,13 @@ public class MainActivity2 extends AppCompatActivity {
             CityName = city;
             get7daysData(CityName);
         }
+    }
+    private void anhxa(){
+        imgback=(ImageView) findViewById(R.id.imageBack);
+        txtName=(TextView) findViewById(R.id.dailyWeather);
+        lv=(ListView) findViewById(R.id.listview);
+        weatherArrayList = new ArrayList<Weather>();
+        customAdapter = new CustomAdapter(MainActivity2.this,weatherArrayList);
     }
     private void get7daysData(String data) {
         String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + data + "&cnt={cnt}&appid=22de7a243950349ac3f822b7f06b2cb1";
@@ -48,4 +66,3 @@ public class MainActivity2 extends AppCompatActivity {
                 });
         requestQueue.add(stringRequest);
     }
-}
